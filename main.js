f869e34843ab062e84ffa871bb7cac382b772d63
Daniel Devarajan (Contractor) <Daniel.Devarajan@wnco.com>
2024-11-19 14:27:21 -0600

    Committing for security. Will go into detail later

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 3000
 const connection = require("./DATABASE/server.js");
 
 
-
+const timer = ms => new Promise(res => setTimeout(res, ms));
 
 
 
@@ -313,12 +313,14 @@ bibleDataTEST = [
   }
 ]
 bibleDataTEST1 = [
+  
   {
-    Book: "Matthew",
-    Chapters: 28
+    Book: "Mark",
+    Chapters: 1
   }
 ]
-bibleDataTEST1.forEach(function (arrayItem){
+bibleDataTEST1.forEach( async function (arrayItem){
+  await timer(5000);
   let x = arrayItem.Book;
   let y = arrayItem.Chapters;
   let i = 1;
@@ -326,9 +328,10 @@ bibleDataTEST1.forEach(function (arrayItem){
 
   //switch back to i <= y when done
     while( i <= y){
+      await timer(3000);
       let chapterNumber = i;
-      agentURL = `https://read.lsbible.org/?q=${"matthew"}+${i}`;
-      console.log(`Book:${x} Chapter:${i}`)
+      agentURL = `https://read.lsbible.org/?q=${x}+${i}`;
+      console.log(`                                                       Book:${x} Chapter:${i}`)
 
 
       axios.get(agentURL)
@@ -337,25 +340,189 @@ bibleDataTEST1.forEach(function (arrayItem){
         $(".verse").each((index, element) => {
           let verses = "";
           let redVerses = "";
+          let blockQuote = "";
+          let italics = "";
           let numberofVerse = $(element).attr("data-key");
+          let numberofVerse2 = $(element).find("[data-verse]").text();
+          
 
           let subHead;
+          let updatedText;
+
+
           
-          if ($(element).find(".prose").text() === ""){
-            if ($(element).find(".red-letter").text() != ""){
-              $(element).find(".red-letter").each(function(index, content){
-                redVerses = `${redVerses}*${$(content).text()}`;
-              });
+
+          const asteriskedPhrase = (text, phrase, phraseWithAsterisks) => {
+            const words = text.split(" ");
+            let result = [];
+            // console.log(`                                                                                                         `)
+            // console.log(`|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||`)
+            // console.log(`|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||`)
+            // console.log(`|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||`)
+            // console.log(`TEXT SPLIT: ${words}`)
+            // console.log(`FUNCTION: ${text}`)
+            // console.log(`KEY PHRASE: ${phrase.trim()} \ PHRASE LENGTH: ${phrase.split(" ").length}`)
+            // console.log(`UPDATED PHRASE: ${phraseWithAsterisks}`)
+            // console.log(`\\\                                                                                                   ///`)
+            // console.log(`                                               RESULT                                                    `)
+            console.log(``)
+
+            for (let i = 0; i < words.length; i++){
+              if ( words.slice(i, i + phrase.trim().split(" ").length).join(" ") === phrase.trim()) {
+                result.push(phraseWithAsterisks);
+                i += phrase.split(" ").length - 1;
+              }else{
+                result.push(words[i]);
+              }
             }
-            verses = $(element).find(".poetry").text();
-          }else if ($(element).find(".poetry").text() === ""){
-            if ($(element).find(".red-letter").text() != ""){
-              $(element).find(".red-letter").each(function(index, content){
-                redVerses = `${redVerses}*${$(content).text()}`;
-              });
+            
+            return result.join(" ");
+          };
+
+
+          
+
+          let numberCounter = 0;
+          $(element).each((index, parent) => {
+            
+            const firstChild = $(parent).children();
+            let firstTree = $(parent)
+            // console.log(numberCounter)
+            // console.log(firstTree.children().eq(numberCounter).attr("class"))
+             
+            
+            // console.log(`First child of parent ${index + 1}:`, firstChild.text());
+            // console.log(``);
+
+
+            let y = 10;
+            let i = 0;
+            let textCompiledVariable = "empty";
+            console.log(`||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||`);
+            console.log(`||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||`);
+            console.log(`||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||`);
+            console.log(`||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||`);
+            console.log(`||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||`);
+            console.log(`||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||`);
+
+            while (i < y){
+              if (firstTree.children().eq(i).attr("class") === "prose" || firstTree.children().eq(i).attr("class") === "poetry" || firstTree.children().eq(i).attr("class") === "small-caps")
+              {
+                console.log(`-----------------------------------------------W/LOOP LOGIC TRIGGERED-----------------------------------`);
+                if(firstTree.children().eq(i).text() != undefined || firstTree.children().eq(i).text() != ""){
+                  textCompileVariable = textCompiledVariable + firstTree.children().eq(i).text();
+                  console.log(firstTree.children().eq(i).text())
+                }
+
+                
+              }
+              console.log(i)
+              i++;
+              console.log(`CLASS: ${firstTree.children().eq(i).attr("class")}`)
+              console.log(`TEXT: ${firstTree.children().eq(i).text()}`)
+              
+              
             }
-            verses = $(element).find(".prose").text();
+            
+            
+            //   else if (firstTree.children().eq(index).attr("class") === "float"){
+                
+            //   }
+
+
+              numberCounter = numberCounter + 1;
+            })
+          
+          
+
+
+
+          if ($(element).find(".block-quote").text()){
+            
+
+            verses = $(element).find(".block-quote").text();
           }
+          else if ($(element).find(".poetry").text()){
+
+            if ( $(element).find("i") != ""){
+              
+              let italics = $(element).find("i").text();
+              
+              let italicsAsterisk = `&${italics}&`;
+
+              
+              updatedText = asteriskedPhrase( $(element).find(".poetry").text(), italics, italicsAsterisk);
+
+
+              verse = updatedText
+
+              // console.log(`UPDATED ASTERISKED TEXT:  ${updatedText}`);
+            }
+            if ( $(element).find(".small-caps") ){
+              // console.log("SM FOUND"); 
+            }
+
+            if ($(element).find(".red-letter").text() != ""){
+              $(element).find(".red-letter").each(function(index, content){
+                redVerses = `${redVerses}*${$(content).text()}`;
+              });
+
+            };
+
+            
+
+            if (verses === updatedText){
+              verses = updatedText;
+            }else{
+              // console.log(` TEST PRINT:${$(element).find(".poetry").textContent} `)
+              verses = $(element).find(".poetry").text();
+            }
+
+
+
+          }else if ($(element).find(".prose").text()){
+
+            if ( $(element).find("i") != ""){
+              
+              let italics = $(element).find("i").text();
+              
+              let italicsAsterisk = `&${italics}&`;
+
+              
+              updatedText = asteriskedPhrase( $(element).find(".prose").text(), italics, italicsAsterisk);
+
+
+              
+              
+              
+              // console.log(`UPDATED ASTERISKED TEXT: ${updatedText}`);
+              verses = updatedText;
+            }
+            if ( $(element).find(".small-caps") ){
+              // console.log("SM FOUND"); 
+            }
+
+            if ($(element).find(".red-letter").text() != ""){
+              $(element).find(".red-letter").each(function(index, content){
+                redVerses = `${redVerses}*${$(content).text()}`;
+              });
+              
+            };
+
+
+
+            if (verses === updatedText){
+              verses = updatedText;
+            }else{
+            
+             verses = $(element).find(".prose").text();
+            //  console.log(`VERSE: ${verses}`)
+            }
+
+ 
+
+          }
+
 
           //SUBHEADER FINDER/PRINTER
           if ( $(element).find(".subhead").text() != "" || $(element).find(".subhead").text() != "undefined" || $(element).find(".subhead").text() != null || $(element).find(".subhead").text() != undefined ){
@@ -368,19 +535,20 @@ bibleDataTEST1.forEach(function (arrayItem){
           
           let versesWithoutCommas = verses.replace(/,/g,"<^>");
           let adjustedNumberofVerse = numberofVerse.replace(/-/g,'', 'hex');
-          let strippedAdjustedNumberV = adjustedNumberofVerse.substring(5,2);
-          let test = "ABC";
+          let strippedAdjustedNumberV = adjustedNumberofVerse.substring(5,8);
+          let declaredVerseNumber = numberofVerse2;
+          
 
           
           let sqlStatement4 = "INSERT INTO `scrapeddata` (`idscrapedData`, `book`, `subHead`, `chapter`, `verseNumber`, `verse`, `redVerse`) VALUES ('"+uuidv6()+"','"+x+"', '"+subHead+"','"+chapterNumber+"',"+strippedAdjustedNumberV+", '"+verses+"', '"+redVerses+"')";
-          console.log(sqlStatement4);
+          // console.log(sqlStatement4);
           
-          console.log(adjustedNumberofVerse.substring(5,2));
+          
           connection.query(sqlStatement4, function (error, result){
             if (error){
               throw error;
             }else{
-              console.log ("RECORD INSERTED!@");
+              console.log ("                                                                                                                    RECORD INSERTED!@");
             }
           });
 
